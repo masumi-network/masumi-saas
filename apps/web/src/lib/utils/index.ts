313f8172106c21e306052f1eeb99d0fb005c40c3
Isaac. <bytegenlabs@gmail.com>
2026-02-12 17:55:39 +0100

    fix: sort utils index exports for simple-import-sort

--- apps/web/src/lib/utils/index.ts
+++ apps/web/src/lib/utils/index.ts
@@ -1,4 +1,4 @@
 export { cn } from "./cn";
-export { formatDate, formatRelativeDate } from "./format-date";
 export { getGreeting } from "./date";
+export { formatDate, formatRelativeDate } from "./format-date";
 export { formatName, getInitials } from "./format-name";
